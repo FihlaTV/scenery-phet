68db90c66d8fe5b4e058256e489d9447e7d4591b
jbphet <john.blanco@colorado.edu>
2019-08-21 13:23:38 -0600

    added a singleton that creates and makes available common UI sound players, see https://github.com/phetsims/sun/issues/506 aand https://github.com/phetsims/sun/issues/505

--- js/buttons/ResetAllButton.js
+++ js/buttons/ResetAllButton.js
@@ -13,6 +13,7 @@ define( function( require ) {
   // modules
   var ActivationUtterance = require( 'SCENERY_PHET/accessibility/ActivationUtterance' );
   var BooleanIO = require( 'TANDEM/types/BooleanIO' );
+  var commonSoundPlayers = require( 'TAMBO/commonSoundPlayers' );
   var DerivedProperty = require( 'AXON/DerivedProperty' );
   var DerivedPropertyIO = require( 'AXON/DerivedPropertyIO' );
   var inherit = require( 'PHET_CORE/inherit' );
@@ -20,26 +21,15 @@ define( function( require ) {
   var ResetButton = require( 'SCENERY_PHET/buttons/ResetButton' );
   var sceneryPhet = require( 'SCENERY_PHET/sceneryPhet' );
   var SceneryPhetA11yStrings = require( 'SCENERY_PHET/SceneryPhetA11yStrings' );
-  var SoundClip = require( 'TAMBO/sound-generators/SoundClip' );
-  var soundManager = require( 'TAMBO/soundManager' );
   var Tandem = require( 'TANDEM/Tandem' );
   var utteranceQueue = require( 'SCENERY_PHET/accessibility/utteranceQueue' );
 
   // constants
   var RESET_ALL_BUTTON_RADIUS = 20.8;
 
-  // sounds
-  const defaultResetAllSound = require( 'sound!TAMBO/reset-all.mp3' );
-
   // a11y strings - not translatable
   var resetAllButtonNameString = SceneryPhetA11yStrings.resetAllLabelString.value;
   var resetAllAlertString = SceneryPhetA11yStrings.resetAllAlertString.value;
-
-  // singleton variables used to avoid multiple allocations of the reset all sound clip
-  var defaultSoundClipInfo = {
-    soundClip: null,
-    referenceCount: 0
-  };
 
   /**
    * @param {Object} [options]
@@ -59,7 +49,7 @@ define( function( require ) {
       phetioDocumentation: 'The orange, round button that can be used to restore the initial state',
 
       // sound, supply an alternative if desired or set to null for no sound
-      soundInfo: defaultResetAllSound,
+      soundGenerationStrategy: resetAllSoundGenerationStrategy,
 
       // options for sound generation
       soundOptions: { initialOutputLevel: 0.7 },
@@ -78,35 +68,6 @@ define( function( require ) {
       phetioState: false // this is a transient property based on user interaction, should not be stored in the state
     } );
 
-    // sound generation - only loaded if sound is on (to conserve memory and load time)
-    // TODO: this is not finalized, see https://github.com/phetsims/tambo/issues/73
-    var resetAllSoundClip = null;
-    if ( phet.joist.sim.supportsSound && options.soundInfo !== null ) {
-      if ( options.soundInfo === defaultResetAllSound ) {
-        if ( !defaultSoundClipInfo.soundClip ) {
-
-          // this is the first usage of this sound clip, so create and register it
-          defaultSoundClipInfo.soundClip = new SoundClip( defaultResetAllSound, { initialOutputLevel: 0.7 } );
-          soundManager.addSoundGenerator( defaultSoundClipInfo.soundClip );
-        }
-        defaultSoundClipInfo.referenceCount++;
-        resetAllSoundClip = defaultSoundClipInfo.soundClip;
-      }
-      else {
-
-        // the client has specified a non-default sound clip, create it and hook it up
-        resetAllSoundClip = new SoundClip( options.soundInfo, options.soundOptions );
-        soundManager.addSoundGenerator( resetAllSoundClip );
-      }
-
-      // play the sound when fired
-      isFiringProperty.lazyLink( function( isFiring ) {
-        if ( isFiring ) {
-          resetAllSoundClip.play();
-        }
-      } );
-    }
-
     // a11y - when reset all button is fired, disable alerts so that there isn't an excessive stream of alerts
     // while many Properties are reset. When callbacks are ended for reset all, enable alerts again and announce an
     // alert that everything was reset.
@@ -124,25 +85,28 @@ define( function( require ) {
 
     // @private - dispose function
     this.disposeResetAllButton = function() {
-      if ( resetAllSoundClip ) {
-
-        // if the default sound clip was in use, see if this was the last user and, if so, de-register it
-        if ( resetAllSoundClip === defaultSoundClipInfo.soundClip ) {
-          defaultSoundClipInfo.referenceCount--;
-          if ( defaultSoundClipInfo.referenceCount === 0 ) {
-            soundManager.removeSoundGenerator( resetAllSoundClip );
-            resetAllSoundClip.dispose();
-          }
-        }
-        else {
-          soundManager.removeSoundGenerator( resetAllSoundClip );
-          resetAllSoundClip.dispose();
-        }
-        soundManager.removeSoundGenerator( resetAllSoundClip );
-      }
       isFiringProperty.dispose();
     };
   }
+
+  /**
+   * strategy for playing reset all sound
+   */
+  var resetAllSoundGenerationStrategy = function( buttonModel, fireOnDown ) {
+
+    const resetAllSoundPlayer = commonSoundPlayers.resetAllSoundPlayer;
+
+    var playFiredSound = function() {
+      resetAllSoundPlayer.play();
+    };
+
+    buttonModel.firedEmitter.addListener( playFiredSound );
+
+    // dispose function
+    this.dispose = function() {
+      buttonModel.firedEmitter.removeListener( playFiredSound );
+    };
+  };
 
   sceneryPhet.register( 'ResetAllButton', ResetAllButton );
 
