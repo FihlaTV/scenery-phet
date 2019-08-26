4cd705fd6efd4adc22f7b32a7f020daf84d96513
jbphet <john.blanco@colorado.edu>
2019-08-26 17:37:16 -0600

    added sound generation for checkboxes that uses a single instance of the sound clip, see https://github.com/phetsims/tambo/issues/73

--- js/buttons/ResetAllButton.js
+++ js/buttons/ResetAllButton.js
@@ -52,9 +52,6 @@ define( function( require ) {
       // sound production is desired
       soundPlayer: commonSoundPlayers.resetAllSoundPlayer,
 
-      // options for sound generation
-      soundOptions: { initialOutputLevel: 0.7 },
-
       // a11y
       innerContent: resetAllButtonNameString
     }, options );
